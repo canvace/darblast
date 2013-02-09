23f29831d9f2c1eb8fc87e302a9466f5b1e49820
Alberto La Rocca <a71104@gmail.com>
2013-02-09 04:10:15 +0100

    Rendering main view.

--- src/canvace.js
+++ src/canvace.js
@@ -119,9 +119,20 @@ if (users !== null) {
 	}));
 }
 
+app.get('/', function (request, response) {
+	response.render('main.handlebars');
+});
+
 (function (port) {
-	app.listen(port, function () {
-		console.log('Canvace Development Environment running on port ' + port);
-		require('openurl').open('http://localhost:' + port + '/');
-	});
+	if (isFinite(port)) {
+		app.listen(port, function () {
+			console.log('Canvace Development Environment running on port ' + port);
+			require('openurl').open('http://localhost:' + port + '/');
+		});
+	} else {
+		app.listen(80, function () {
+			console.log('Canvace Development Environment running on port 80');
+			require('openurl').open('http://localhost/');
+		});
+	}
 }(parseInt(config.port, 10)));
