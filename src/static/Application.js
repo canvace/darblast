fe2db2c0d73df117e35ba648fdfecf0fdbdf9f31
Alberto La Rocca <a71104@gmail.com>
2013-02-11 19:55:28 +0100

    Left toolbar.

--- src/static/Application.js
+++ src/static/Application.js
@@ -2,7 +2,46 @@ Ext.application({
 	name: 'Canvace Development Environment',
 	launch: function () {
 		Ext.create('Ext.container.Viewport', {
-			layout: 'fit'
+			layout: 'border',
+			items: [{
+				region: 'center'
+			}, {
+				xtype: 'toolbar',
+				region: 'west',
+				layout: 'vbox',
+				defaults: {
+					scale: 'large'
+				},
+				items: [{
+					icon: 'resources/images/tools/drag.png'
+				}, {
+					xtype: 'splitbutton',
+					icon: 'resources/images/tools/stamp.png',
+					items: [{
+						icon: 'resources/images/tools/stamptiles.png'
+					}, {
+						icon: 'resources/images/tools/stampentities.png'
+					}]
+				}, {
+					xtype: 'splitbutton',
+					icon: 'resources/images/tools/fill.png'
+				}, {
+					icon: 'resources/images/tools/move_entity.png'
+				}, {
+					xtype: 'splitbutton',
+					icon: 'resources/images/tools/erase.png'
+				}, {
+					xtype: 'splitbutton',
+					icon: 'resources/images/tools/select.png'
+				}, {
+					xtype: 'splitbutton',
+					icon: 'resources/images/tools/clipboard.png'
+				}]
+			}, {
+				region: 'east'
+			}, {
+				region: 'south'
+			}]
 		});
 	}
 });
