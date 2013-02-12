39506acd644afd3fbeb73dab4cb5da876291338b
Alberto La Rocca <a71104@gmail.com>
2013-02-13 00:12:00 +0100

    AJAX wrapper.

--- src/static/Canvace.js
+++ src/static/Canvace.js
@@ -8,5 +8,29 @@ var Canvace = (function () {
 			initHandlers[i](components);
 		}
 	};
+	function bindAjax(method) {
+		return function (url, data, callback) {
+			if (arguments.length < 3) {
+				callback = data;
+			}
+			var settings = {
+				url: url,
+				method: method,
+				callback: function (response) {
+					callback(JSON.parse(response.responseText));
+				}
+			};
+			if (arguments.length > 2) {
+				settings.params = data;
+			}
+			Ext.Ajax.request(settings);
+		};
+	}
+	thisObject.Ajax = {
+		get: bindAjax('GET'),
+		post: bindAjax('POST'),
+		put: bindAjax('PUT'),
+		_delete: bindAjax('DELETE')
+	};
 	return thisObject;
 }());
