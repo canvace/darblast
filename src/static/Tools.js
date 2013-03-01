51ae207a25c449c4460c832ffcf76067193272ec
Alberto La Rocca <a71104@gmail.com>
2013-03-01 16:25:00 +0100

    Canvas mouse events

--- src/static/Tools.js
+++ src/static/Tools.js
@@ -1,5 +1,31 @@
 function Tools() {
-	new DragTool();
+	var activeTool = new DragTool();
 
 	// TODO
+
+	var canvas = Ext.get('canvas');
+	var down = false;
+	canvas.on('mousemove', function (event) {
+		if (down) {
+			if (activeTool.mousedrag) {
+				activeTool.mousedrag(event.getX(), event.getY());
+			}
+		} else {
+			if (activeTool.mousemove) {
+				activeTool.mousemove(event.getX(), event.getY());
+			}
+		}
+	});
+	canvas.on('mousedown', function (event) {
+		down = true;
+		if (activeTool.mousedown) {
+			activeTool.mousedown(event.getX(), event.getY());
+		}
+	});
+	canvas.on('mouseup', function (event) {
+		if (activeTool.mouseup) {
+			activeTool.mouseup(event.getX(), event.getY());
+		}
+		down = false;
+	});
 }
