8244af898941fda01be005f0f9ef30199efcc0a8
Alberto La Rocca <a71104@gmail.com>
2013-03-10 18:52:28 +0100

    Changing default port to 7104

--- src/Epilogue.js
+++ src/Epilogue.js
@@ -1,15 +1,11 @@
 /*global config: false, server: false */
 
 (function (port) {
-	if (isFinite(port)) {
-		server.listen(port, function () {
-			console.log('Canvace Development Environment running on port ' + port);
-			require('openurl').open('http://localhost:' + port + '/');
-		});
-	} else {
-		server.listen(80, function () {
-			console.log('Canvace Development Environment running on port 80');
-			require('openurl').open('http://localhost/');
-		});
+	if (!isFinite(port)) {
+		port = 7104;
 	}
+	server.listen(port, function () {
+		console.log('Canvace Development Environment running on port ' + port);
+		require('openurl').open('http://localhost:' + port + '/');
+	});
 }(parseInt(config.port, 10)));
