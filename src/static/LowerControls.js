f46a495888f8bde033cacdc5843cdd6657a14819
Alberto La Rocca <a71104@gmail.com>
2013-03-13 00:59:02 +0100

    UI

--- src/static/LowerControls.js
+++ src/static/LowerControls.js
@@ -89,7 +89,7 @@ var LowerControls = (function () {
 						} else {
 							Ext.MessageBox.show({
 								title: 'Additional software needed',
-								msg: 'To import image sheets into Canvace you need to install Cairo and restart the environment.<br/>Do you want to open the Cairo website now? (Another window will open)',
+								msg: 'To import image sheets into Canvace you need to install Cairo and restart the environment.<br/>Do you want to open the Cairo website now? (Another browser window will open)',
 								buttons: Ext.MessageBox.YESNO,
 								icon: Ext.MessageBox.INFORMATION,
 								fn: function (button) {
