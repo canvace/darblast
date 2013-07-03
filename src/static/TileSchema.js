0a663ebfce18b994888c5cf8b7f6e3e6df017fe7
Alberto La Rocca <a71104@gmail.com>
2013-07-03 13:05:01 +0200

    Tile layout configuration schema

--- src/static/TileSchema.js
+++ src/static/TileSchema.js
@@ -18,47 +18,47 @@
  *	along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-Ext.define('Darblast.ux.TileSchema', (function () {
-	var containerId = Ext.id();
-	var iSpan = 1;
-	var jSpan = 1;
+Ext.define('Darblast.ux.TileSchema', {
+	extend: 'Ext.Component',
+	alias: 'widget.tileschema',
 
-	return {
-		extend: 'Ext.Component',
-		alias: 'widget.tileschema',
+	constructor: function (config) {
+		config = config || {};
+		var containerId = Ext.id();
+		config.html = '<div id="' + containerId + '" style="padding: 24px"><div class="offset" style="position: relative; left: 0px; top: 0px"></div></div>';
+		this.callParent([config]);
+		this.containerId = containerId;
+		this.iSpan = 1;
+		this.jSpan = 1;
+		this.on('afterrender', function () {
+			this.setSpan(1, 1);
+		});
+	},
 
-		constructor: function (config) {
-			config = config || {};
-			config.html = '<div id="' + containerId + '" style="padding: 24px; position: relative; left: 0px; top: 0px"></div>';
-			this.callParent([config]);
-			this.on('afterrender', function () {
-				this.setSpan(iSpan, jSpan);
-			});
-		},
+	getSpan: function () {
+		return {
+			i: this.iSpan,
+			j: this.jSpan
+		};
+	},
 
-		getSpan: function () {
-			return {
-				i: iSpan,
-				j: jSpan
-			};
-		},
-
-		setSpan: function (i, j) {
-			iSpan = i;
-			jSpan = j;
-			var container = document.querySelector('#' + containerId);
-			var oldCanvas = document.querySelector('#' + containerId + ' canvas');
-			var newCanvas = Canvace.view.generateBox(i, j, 1);
-			newCanvas.style.position = 'absolute';
-			newCanvas.style.left = 0;
-			newCanvas.style.top = 0;
-			if (oldCanvas) {
-				container.replaceChild(oldCanvas, newCanvas);
-			} else {
-				container.appendChild(newCanvas);
-				// TODO place cursor
-			}
-			this.setSize(container.clientWidth, container.clientHeight);
+	setSpan: function (i, j) {
+		this.iSpan = i;
+		this.jSpan = j;
+		var container = document.querySelector('#' + this.containerId);
+		var offset = document.querySelector('#' + this.containerId + ' .offset');
+		var oldCanvas = document.querySelector('#' + this.containerId + ' .offset canvas');
+		var newCanvas = Canvace.view.generateBox(i, j, 0);
+		if (oldCanvas) {
+			offset.replaceChild(oldCanvas, newCanvas);
+		} else {
+			offset.appendChild(newCanvas);
+			var cursor = Canvace.view.generateTileHighlight();
+			cursor.style.position = 'absolute';
+			cursor.style.left = 0;
+			cursor.style.top = 0;
+			offset.appendChild(cursor);
 		}
-	};
-}()));
+		this.setSize(container.clientWidth, container.clientHeight);
+	}
+});
